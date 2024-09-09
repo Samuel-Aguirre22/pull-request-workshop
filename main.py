c8b5d35ac4e9d3d4c14f4812953508295ba54904
jpenuela420 <jpenuela560@soyudemedellin.edu.co>
2024-09-09 09:43:05 -0500

    lista de contactos

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 # El siguiente programa recibe una entrada (input) del usuario y retorna el número de dígitos y de letras que contenga el mismo
 
 # Itendifica los errores y modifica el programa para que funcione según el enunciado
-user_input = "Dan1el"
+user_input = input("escribe un nombre con letras y digitos :")
 
 digits = 0
 letters = 0
 
 for i in user_input:
     if i.isdigit():
-        digits = 0
+        digits = digits+1
     elif i.isalpha():
-        letters = 0
+        letters = letters+1
 
 print(" The input string", user_input, "has", letters, "letters and", digits, "digits.")
